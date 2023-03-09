7bb530ab988fef9bf0440f8f8e514cd52d028b0c
Qizhao Chen <2766657C@student.gla.ac.uk>
2023-03-09 18:29:06 +0000

    Add a basic login page, student information page and editing functions, and Bootstrap and encrypt utils classes

--- projectITECH/projectITECH/settings.py
+++ projectITECH/projectITECH/settings.py
@@ -47,6 +47,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "rmc.middleware.auth.AuthMiddleware",
 ]
 
 ROOT_URLCONF = "projectITECH.urls"
